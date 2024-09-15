9b44eb1c9ab4e652f6f2700996c4e73d982dfbe9
osw6858 <osw7875@naver.com>
2024-09-15 19:41:38 +0900

    :lipstick: 회원가입 단계 표시 인디케이터 컴포넌트 추가

--- src/app/[locale]/(auth)/signup/page.tsx
+++ src/app/[locale]/(auth)/signup/page.tsx
@@ -1,13 +1,15 @@
 import SignUpProgressIndicator from '@/app/[locale]/(auth)/_components/SignUpProgressIndicator';
+import SignUpForm from '@/app/[locale]/(auth)/_components/SignUpForm';
 
 export default function SignUpPage() {
   return (
     <>
       <h1 className={'mb-5 text-3xl font-bold '}>Logo</h1>
-      <div className={'flex gap-3'}>
+      <div className={'flex items-center gap-3 mb-14'}>
         <SignUpProgressIndicator />
-        <h2 className={'text-2xl font-bold mb-14'}>이메일로 가입하기</h2>
+        <h2 className={'text-2xl font-bold '}>이메일로 가입하기</h2>
       </div>
+      <SignUpForm />
     </>
   );
 }
