110c47d09abf9b8d4e5ee403e66608620d4cd5b0
osw6858 <osw7875@naver.com>
2024-09-17 18:43:21 +0900

    :truck: font다운받은 폴더를 src폴더로 이동

--- src/lib/fonts.ts
+++ src/lib/fonts.ts
@@ -3,22 +3,22 @@ import localFont from 'next/font/local';
 export const pretendard = localFont({
   src: [
     {
-      path: '../app/fonts/Pretendard-Regular.woff2',
+      path: '../fonts/Pretendard-Regular.woff2',
       weight: '400',
       style: 'normal',
     },
     {
-      path: '../app/fonts/Pretendard-Medium.woff2',
+      path: '../fonts/Pretendard-Medium.woff2',
       weight: '500',
       style: 'normal',
     },
     {
-      path: '../app/fonts/Pretendard-SemiBold.woff2',
+      path: '../fonts/Pretendard-SemiBold.woff2',
       weight: '600',
       style: 'normal',
     },
     {
-      path: '../app/fonts/Pretendard-Bold.woff2',
+      path: '../fonts/Pretendard-Bold.woff2',
       weight: '700',
       style: 'normal',
     },
